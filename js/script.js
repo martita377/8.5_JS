4dc7eaf1c1ae6e14736a33b72613cef08795c548
Marta P <mapa.startuj@gmail.com>
2017-09-22 22:29:27 +0200

    added new files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Triangle Area - js
+
+// scripts.js
+
+var base = 3;
+var	height = 4;
+var	traingleArea;
+
+var base = prompt('Enter field base: only numbers (decimal separator - use dot)');
+var height = prompt('Enter height - only numbers (decimal separator - use dot)');
+
+triangleArea = base * height / 2;
+
+alert('Triangle field with base a: ' + base + ' and height h: ' + height + ' is equal to: ' + triangleArea);
+
+console.log('Triangle field with base a: ' + base + ' and height h: ' + height + ' is equal to: ' + triangleArea);
+
