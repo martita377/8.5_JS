71befca5bccebd9b5863acb160842640939c4d1f
Marta P <mapa.startuj@gmail.com>
2017-09-22 22:39:56 +0200

    modified var

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 
 // scripts.js
 
-var base = 3;
-var	height = 4;
-var	traingleArea;
+var base = 3,
+	height = 4,
+	traingleArea;
 
-var base = prompt('Enter field base: only numbers (decimal separator - use dot)');
-var height = prompt('Enter height - only numbers (decimal separator - use dot)');
+var base = prompt('Enter field base: only numbers (decimal separator - use dot)'),
+	height = prompt('Enter height - only numbers (decimal separator - use dot)');
 
 triangleArea = base * height / 2;
 
